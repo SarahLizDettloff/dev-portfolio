bf688369a355acbe626d96c1404b287eb8f38264
Sarah Liz Dettloff <Sarah.Liz.Dettloff@gmail.com>
2019-07-16 22:58:49 -0400

    Updating routing

--- src/props/navbar.js
+++ src/props/navbar.js
@@ -1,44 +1,31 @@
 import React from 'react';
-import { HashRouter, Route, Link} from "react-router-dom";
-
-import Home from '../components/homeComp.js';
-import About from '../components/aboutComp.js';
-import Development from '../components/developmentComp.js';
 
 import '../styles/NavStyle.css';
 
 const NavBar = props => (
-	<HashRouter basename="/">
     <ul class="block-menu" background-color="#581845">
-	<Link to="/">
-	<li><a href="dev-portfolio/#" class="three-d">
+	<li><a href="/" class="three-d">
 		Home
 		<span aria-hidden="true" class="three-d-box">
 			<span class="front">Home</span>
 			<span class="back">Home</span>
 		</span>
-	</a></li></Link>
-	<Link to="#/about">
+	</a></li>
   <li><a href="#/about" class="three-d">
   About
 		<span aria-hidden="true" class="three-d-box">
 			<span class="front">About</span>
 			<span class="back">About</span>
 		</span>
-	</a></li></Link>
-	<Link to="#/development">
+	</a></li>
   <li><a href="#/development" class="three-d">
   Development
 		<span aria-hidden="true" class="three-d-box">
 			<span class="front">Development</span>
 			<span class="back">Development</span>
 		</span>
-	</a></li></Link>
+	</a></li>
 </ul>
-      <Route exact path="/" component={Home} />
-      <Route path="#/about" component={About} />
-      <Route path="#/development" component={Development} />
-</HashRouter>
   );
   
   export default NavBar;
