38a41cd30b350180741000eabeb3511e3de4d48a
Sarah Liz Dettloff <Sarah.Liz.Dettloff@gmail.com>
2019-07-16 22:46:08 -0400

    Routing fix for navbar

--- src/props/navbar.js
+++ src/props/navbar.js
@@ -4,21 +4,21 @@ import '../styles/NavStyle.css';
 
 const NavBar = props => (
     <ul class="block-menu" background-color="#581845">
-	<li><a href="/" class="three-d">
+	<li><a href="dev-portfolio/#" class="three-d">
 		Home
 		<span aria-hidden="true" class="three-d-box">
 			<span class="front">Home</span>
 			<span class="back">Home</span>
 		</span>
 	</a></li>
-  <li><a href="#/about" class="three-d">
+  <li><a href="dev-portfolio/#/about" class="three-d">
   About
 		<span aria-hidden="true" class="three-d-box">
 			<span class="front">About</span>
 			<span class="back">About</span>
 		</span>
 	</a></li>
-  <li><a href="#/development" class="three-d">
+  <li><a href="dev-portfolio/#/development" class="three-d">
   Development
 		<span aria-hidden="true" class="three-d-box">
 			<span class="front">Development</span>
