b1c8137c31b9f6b81b7f042f1dd78be863138b83
Sarah Liz Dettloff <Sarah.Liz.Dettloff@gmail.com>
2020-04-05 17:03:01 -0400

    AdjustingMarginsandFontSize

--- src/components/developmentComp.js
+++ src/components/developmentComp.js
@@ -1,6 +1,4 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
-import { render } from 'react-dom';
 import Gallery from 'react-grid-gallery';
 
 import '../styles/DevStyle.css';
--- src/components/explore.js
+++ src/components/explore.js
@@ -1,9 +1,6 @@
 import React from 'react';
-import Fade from 'react-reveal/Fade';
 
 import Shape from '../components/homeScene.js';
-import WelcomeAnimations from './bounce';
-import fawn from '../assets/fawn.gif';
 
 import '../styles/HomeStyle.css';
 
