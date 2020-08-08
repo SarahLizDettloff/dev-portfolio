fda62c527d116e671b429a66198a1319e27c917b
unknown <sarah.liz.dettloff@gmail.com>
2020-08-08 17:03:30 -0400

    Updates

---
+++ precache-manifest.29bc7891fa85c292329c9b8ccd2f4ef5.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41deea98cbf34669c2722c86c59eae63",
+    "url": "/dev-portfolio/index.html"
+  },
+  {
+    "revision": "9164e3717cb93f25f8b5",
+    "url": "/dev-portfolio/static/css/main.6b38fc01.chunk.css"
+  },
+  {
+    "revision": "f7d77e65db8bd591593f",
+    "url": "/dev-portfolio/static/js/2.29225fae.chunk.js"
+  },
+  {
+    "revision": "6383bfbb53580fb28a3bc466d3267ab3",
+    "url": "/dev-portfolio/static/js/2.29225fae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9164e3717cb93f25f8b5",
+    "url": "/dev-portfolio/static/js/main.4be593aa.chunk.js"
+  },
+  {
+    "revision": "1e10e024f0899dac1144",
+    "url": "/dev-portfolio/static/js/runtime-main.ce1c534e.js"
+  },
+  {
+    "revision": "72dab0d91123583955144b4938a2b137",
+    "url": "/dev-portfolio/static/media/basiccalculator.72dab0d9.png"
+  },
+  {
+    "revision": "a7849efc60fa30fa04d1064818836afc",
+    "url": "/dev-portfolio/static/media/cheeseOnThatScreenshot.a7849efc.png"
+  },
+  {
+    "revision": "168d2571bd52c6dbfc4d205145e713e5",
+    "url": "/dev-portfolio/static/media/chinese_garden.168d2571.png"
+  },
+  {
+    "revision": "0f788ec43744448103e3deea909cb0b0",
+    "url": "/dev-portfolio/static/media/donut.0f788ec4.png"
+  },
+  {
+    "revision": "4ecdf72e911a2b6e1b51376f47654418",
+    "url": "/dev-portfolio/static/media/kiaraDawn.4ecdf72e.png"
+  },
+  {
+    "revision": "24a17ab7fa4330708bf31476deb615b1",
+    "url": "/dev-portfolio/static/media/lakeSide.24a17ab7.png"
+  },
+  {
+    "revision": "3722ecd5edf3248e7aa2023d1785d88d",
+    "url": "/dev-portfolio/static/media/leadenhall_market.3722ecd5.png"
+  },
+  {
+    "revision": "0254b13d6fc4b109cb5f1b2fc6f4ce11",
+    "url": "/dev-portfolio/static/media/linegraph.0254b13d.png"
+  },
+  {
+    "revision": "8922f60587f4aaba69c50f7e36967356",
+    "url": "/dev-portfolio/static/media/lyric_analysis.8922f605.png"
+  },
+  {
+    "revision": "625969e8451a7f759a6017cd19397700",
+    "url": "/dev-portfolio/static/media/marbleScreenshot.625969e8.png"
+  },
+  {
+    "revision": "89bc3938ab1b042ece41e831b134ee9f",
+    "url": "/dev-portfolio/static/media/me.89bc3938.png"
+  },
+  {
+    "revision": "76b30381bf395c65c052a5e2e12bf382",
+    "url": "/dev-portfolio/static/media/molar.76b30381.png"
+  },
+  {
+    "revision": "68cd5a31c163491fccf8c48aa29ea05a",
+    "url": "/dev-portfolio/static/media/mt.68cd5a31.png"
+  },
+  {
+    "revision": "2d8b123a10a9c5754fac0cd26ec91685",
+    "url": "/dev-portfolio/static/media/mutianyu.2d8b123a.png"
+  },
+  {
+    "revision": "53c40e7fab1f22066f60f083bc24e826",
+    "url": "/dev-portfolio/static/media/my_face.53c40e7f.png"
+  },
+  {
+    "revision": "dc66a9da8a3605e2b376a80699c4b0e8",
+    "url": "/dev-portfolio/static/media/physicsApp.dc66a9da.png"
+  },
+  {
+    "revision": "782a69505a6695787e0f94aa3cff536a",
+    "url": "/dev-portfolio/static/media/plate.782a6950.gif"
+  },
+  {
+    "revision": "252bae0d88997c943e3c5b8a2cae155d",
+    "url": "/dev-portfolio/static/media/sinewave.252bae0d.png"
+  },
+  {
+    "revision": "eb2a20cfb479b5dd01f936ce9780711f",
+    "url": "/dev-portfolio/static/media/stock.eb2a20cf.png"
+  },
+  {
+    "revision": "d6286a1958cdf8e4571762ae518e3fb6",
+    "url": "/dev-portfolio/static/media/veniceSunset.d6286a19.png"
+  },
+  {
+    "revision": "b91ebafed0103c4345915f40f65f015e",
+    "url": "/dev-portfolio/static/media/vignaioli_night.b91ebafe.png"
+  },
+  {
+    "revision": "cd48d4e3c5e4e01116896d35c414b961",
+    "url": "/dev-portfolio/static/media/winterEvening.cd48d4e3.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dev-portfolio/precache-manifest.5043415683701b1cf5808de12ba90885.js"
+  "/dev-portfolio/precache-manifest.29bc7891fa85c292329c9b8ccd2f4ef5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/dev-portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
