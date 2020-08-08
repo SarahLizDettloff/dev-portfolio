3f42f7ed8263cb334eb0f0e7d0ab53f1c5b4de5a
unknown <sarah.liz.dettloff@gmail.com>
2020-08-08 16:06:38 -0400

    2.0/Content_and_Style_Upgrade

--- src/components/aboutComp.js
+++ src/components/aboutComp.js
@@ -23,10 +23,26 @@ const About = () =>
 </h1>
  <div class ="about">
  <h3> Hello.</h3>
-  <p>Development allows a place for creativity and problem solving which provides enjoyment in producing game, mobile, web, and software applications. I have two years enterprise experience in web framework development consisting of REST APIs, front end and back end. In my lesure time, I have three years experience in Visual Studio using C#, C++, and Visual Basic and three years in game development working in a wide variety of environments with Game Maker Studio, Godot, and Unity. In the previous three years,  I have also contributed to Hacktoberfest hosted by Digital Ocean (2017, 2018, 2019).
-</p>
+<p>Development allows a place for creativity and problem solving that bring much joy. The act of planning a product, thinking through the logic, coding it, and polishing satisfies my craving for creating. As a developer this carries through with a strive to consistently challenge myself to enhance my abilities.
 <br></br>
-<p>Take a look around, feedback is welcome on this project or any others in my portfolio.</p>
+<br></br>
+I have two and a half years enterprise experience in web framework development consisting of RESTful APIs, front-end, and back-end programming. I am currently employed at Inmar Intelligence as a Test Software Engineer on the Digital Promotions team. There I write functional tests, automation, new features. Collaborating with other teams to help further the companies success can consist of developing new products, setting up alerts, and working through technical challenges to attain the next victory.
+<br></br>
+<br></br>
+Open-source software has been a foundation of programming and growth. In order to give back, I have contributed to Hacktoberfest hosted by Digital Ocean for the previous three years. I have the pleasure to have code in the Arctic Code Vault.
+<br></br>
+<br></br>
+In my leisure time, I enjoy challenging myself in different coding aspects. One of my favorite parts of coding is that your mind is your limit. Game development poses an interesting situation as a developer to compile design, story telling, programming, and fulfill the feel of the game that you have imagined. Unity is my primary choice for a game engine since it gives the user enough freedom to execute their vision, but has built in capabilties to do the heavy lifting for you. Unity is a eminent for android development. React Native along with expo are excellent frameworks for mobile dev that streamline fast development with a smooth finish.
+<br></br>
+<br></br>
+Thank you for stopping by.
+<br></br>
+Feedback is welcome on this project or any others in my portfolio.
+<br></br>
+<br></br>
+<br></br>
+<br></br>
+</p>
 </div>
   </div>
 );
